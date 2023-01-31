0ca43cccd6ede691b74505fe3b5dafe070e64d95
Faiq <ahmadfaiq46@gmail.com>
2023-02-01 01:20:30 +0500

    Search Functionality and Screen Added with Profile Screen Initialized

---
+++ lib/screens/profile_Screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import '../widgets/follow_button.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: Text('Username'),
+        centerTitle: false,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1675095524926-f1a8581615d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80'),
+                      radius: 40,
+                    ),
+                    Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                buildStatColumn(20, 'posts'),
+                                buildStatColumn(150, 'followers'),
+                                buildStatColumn(20, 'following')
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                FollowButton(
+                                  backgroundColor: mobileBackgroundColor,
+                                  borderColor: Colors.grey,
+                                  text: 'Edit Profile Button',
+                                  textColor: primaryColor,
+                                  function: () {},
+                                )
+                              ],
+                            )
+                          ],
+                        )),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Column buildStatColumn(int number, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          number.toString(),
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4),
+          child: Text(
+            label,
+            style: const TextStyle(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/utils/global_Variables.dart
+++ lib/utils/global_Variables.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/profile_Screen.dart';
 import '../screens/add_Post_Screen.dart';
 import '../screens/feed_Screen.dart';
+import '../screens/search_screen.dart';
 
 const webScreen = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('Search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('Notification'),
-  Text('Profile'),
+  ProfileScreen(),
 ];
